77324c63683b766b03dd98706a2c733d29c7c71a
Maureen O'Flynn <maureen.p.oflynn@gmail.com>
2021-12-01 15:23:11 -0800

    Combining Reducers

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,9 +3,13 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 
+
+
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
+
+
 
 class CommentForm extends Component {
     constructor(props) {
@@ -28,8 +32,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log(`Current state is: ${JSON.stringify(values)}`);
-        alert(`Current state is: ${JSON.stringify(values)}`);
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
 
@@ -105,18 +109,19 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                    { comments.map( comment => (
+                    {comments.map(comment => (
                     <div key={comment.id}>
-                    <p>{comment.text}</p>
-                    <p>- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        <p>{comment.text}</p>
+                            <p>- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>
                     </div>
                     ))}
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         )
     }
@@ -139,7 +144,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
